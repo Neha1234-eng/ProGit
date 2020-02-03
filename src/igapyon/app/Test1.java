1aa83c89fd98308150bb4b4e9b12dd8e48ca5dbe
Dell <Dell@DESKTOP-O2E1PSE>
2020-02-03 07:51:46 -0900

    First commit.

---
+++ src/igapyon/app/Test1.java
@@ -0,0 +1,14 @@
+package igapyon.app;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungry="yes";
+		if(hungry=="yes")
+			System.out.println("eat something.....");
+		else
+			System.out.println("Do work......");
+
+	}
+
+}
